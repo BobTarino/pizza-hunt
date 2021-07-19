1bb0bbec6fa10b71658c9f1f4e2641965c4ce144
Bobby T <bob.tarino@gmail.com>
2021-07-19 13:28:30 -0500

    created pizza api routes'

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+// prefix endpoint names and package them up
+const router = require('express').Router();
+const pizzaRoutes = require('./pizza-routes');
+
+// add prefix of `/pizzas` to routes created in `pizza-routes.js`
+router.use('/pizzas', pizzaRoutes);
+
+module.exports = router;
